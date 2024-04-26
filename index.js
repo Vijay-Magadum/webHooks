7a776d72e6a5b008e3260c0587bdc06809be4c14
Vijay-Magadum <143174859+Vijay-Magadum@users.noreply.github.com>
2024-04-26 15:41:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,24 +27,23 @@ app.post('/webhook', (req, res) => {
     res.status(200).send('Event received');
 });
 app.post('/webhooks/stream/custom-commands', async (req, res) => {
-    const { type, channel_id } = req.query; // Assuming channel_id is passed as a query parameter
+    const { type, channel_id } = req.query;
     if (type === 'rfis') {
-        // Create or get a channel
-        const channel = client.channel('formschannel', channel_id);
-
-        // Send message to the channel
-        await channel.sendMessage({
-            text: "Here's your BIM360 link: https://www.autodesk.com/bim-360/",
-            user_id: 'CustomBot' // This should be the ID of a system user or the user who triggered the command
-        });
-
-        console.log("Action Type Received: ", type);
-        res.status(200).send("BIM360 link has been sent");
+        const channel = client.channel('messaging', channel_id);
+        try {
+            await channel.sendMessage({
+                text: "Here's your BIM360 link: https://www.autodesk.com/bim-360/",
+                user_id: 'CustomBot'
+            });
+            res.status(200).send("BIM360 link has been sent");
+        } catch (error) {
+            console.error("Failed to send message:", error);
+            res.status(500).send("Failed to process request");
+        }
     } else {
         res.status(400).send("Unsupported command");
     }
 });
-
 
 
 
